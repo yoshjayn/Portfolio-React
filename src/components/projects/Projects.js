f12cc362e1473e698b720a792cd5e0c15d8cca81
Yashaswini <aswiniy68@gmail.com>
2025-06-11 14:45:05 +0530

    projects updated

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,18 +17,18 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
+          title="Social Media WebApp"
+          des=" Vistagram : A Vite + React Instagram clone with Tailwind CSS. Focus: Context API for state, REST API integration, and learning through iterative builds."
+          src={projectTwo}
+        />
+        <ProjectsCard
           title="VIDEO STREAMING CLONE"
           des=" Experience streaming like never before with my Netflix Clone – a fully responsive web app that replicates the sleek interface and seamless performance of the world's leading entertainment platform."
           src={project1} 
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Revolutionizing online shopping with a dynamic, user-friendly E-commerce Platform – optimized for performance, scalability, and a frictionless shopping experience from browse to checkout."
-          src={projectTwo}
-        />
-        <ProjectsCard
-          title="Ticket Booking App"
-          des=" Showcasing creativity and code in perfect harmony – my Portfolio Website blends aesthetic design with cutting-edge development, serving as a gateway to my professional journey."
+          title="Weather App"
+          des=" This project focuses on fetching and displaying weather information for a specific location and handles various weather data points such as temperature, humidity, pressure, cloud cover, wind speed, and more,by parsing a JSON response from the API and displaying the relevant details"
           src={project3}
         />
         
